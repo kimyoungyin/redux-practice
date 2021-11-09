855a96c796e0150ce8e6da052c4659be43b9d805
kimyoungyin <yin199859@gmail.com>
2021-11-09 21:58:13 +0900

    feat: 투두리스트 with vanillaJS + Redux

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# vanilla-redux 정리 : counter
+# vanilla-redux 정리 : pure redux
 
-## 1. pure redux
+## 1. counter
 
 store: data를 넣는 곳, 즉 state. data를 넣을 수 있는 장소를 생성함
 state: application 내에서 변하는 데이터
@@ -8,7 +8,7 @@ state: application 내에서 변하는 데이터
 createStore: store를 생성. reducer 함수가 인자로 필요함
 createStore.getState() : 데이터를 받음
 
-reducer: 데이터를 변경하는 함수. 오직 이 곳에서만 데이터를 변경할 수 있음.return 값이 application의 데이터가 됨. reducer에서 state를 초기화할 수 있음.
+reducer: 데이터를 변경하는 함수. 오직 이 곳에서만 데이터를 변경할 수 있음. return 값이 application의 데이터가 됨. reducer에서 state를 초기화할 수 있음.
 -> 매개변수 : currentState, action
 
 Action: reducer에서 어떤 걸 보고 state를 변경할까? action! reducer의 두 번쨰 매개변수. 객체여야 한다(type key를 가져야 함).
@@ -18,6 +18,25 @@ subscribe: store 안에 있는 변화들을 알 수 있게 해줌
 정리
 
 1. 데이터를 저장할 store를 state를 변경하는 곳인 reducer
-2. 이를 인자로 한 createStore(reducer)가 return하는 store를 받아 저장
+2. 이를 인자로 한 createStore(reducer) 즉, "store"를 받아 저장
 3. store.dispatch(action) 메서드를 호출하면 reducer에서 매개변수로 받은 이 action 내용에 따라 state 변경
 4. store에 저장된 state가 변경될 때마다 store.subscribe(함수)에 함수가 실행됨.
+
+## 2. ToDoList
+
+-   다른 곳에 갔다가 돌아와도 데이터가 유지되길 바람.
+
+1.  action 객체에 들어갈 프로퍼티는 type 뿐만 아니라 다른 것도 추가 가능함. 사용할 데이터를 넣을 수 있음
+2.  mutate(변형) state 절대 쓰지 말자: [이유](https://redux.js.org/basics/reducers)
+    -> 상태를 수정하는 게 아니라, 새로운 객체 state를 return해야 함.
+
+        ```js
+        // reducer 내부
+
+        // add
+        state.push(action.text) // X
+        return [...state, {text: action.text}] // O
+
+        // delete
+        return state.filter((toDo) => action.id !== toDo.id);
+        ```
--- src/index.js
+++ src/index.js
@@ -1,40 +1,73 @@
 import { createStore } from "redux";
 
-const plus = document.getElementById("add");
-const minus = document.getElementById("minus");
-const number = document.querySelector("span");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-number.innerText = 0;
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-const ADD = "ADD";
-const MINUS = "MINUS";
-
-const countModifier = (count = 0, action) => {
+const reducer = (state = [], action) => {
     switch (action.type) {
-        case ADD:
-            return count + 1;
-        case MINUS:
-            return count - 1;
+        case ADD_TODO:
+            const newToDoObj = { text: action.text, id: action.id };
+            return [newToDoObj, ...state];
+        case DELETE_TODO:
+            const cleaned = state.filter((toDo) => action.id !== toDo.id);
+            return cleaned;
         default:
-            return count;
+            return state;
     }
 };
 
-const countStore = createStore(countModifier);
+const store = createStore(reducer);
 
-const onChange = () => {
-    number.innerText = countStore.getState();
+const addToDo = (text) => {
+    return {
+        type: ADD_TODO,
+        text,
+        id: Date.now(),
+    };
 };
 
-countStore.subscribe(onChange);
-
-const handleAdd = () => {
-    countStore.dispatch({ type: ADD });
+const deleteToDo = (id) => {
+    return {
+        type: DELETE_TODO,
+        id,
+    };
 };
 
-const handleMinus = () => {
-    countStore.dispatch({ type: MINUS });
+const dispatchAddToDo = (text) => {
+    store.dispatch(addToDo(text));
 };
 
-plus.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+const dispatchDeleteToDo = (e) => {
+    const id = parseInt(e.target.parentNode.id);
+    store.dispatch(deleteToDo(id));
+};
+const paintToDos = () => {
+    const toDos = store.getState();
+    ul.innerHTML = "";
+    toDos.forEach((toDo) => {
+        const li = document.createElement("li");
+        const btn = document.createElement("button");
+        btn.innerText = "DEL";
+        btn.addEventListener("click", dispatchDeleteToDo);
+        li.id = toDo.id;
+        li.innerText = toDo.text;
+        li.appendChild(btn);
+        ul.appendChild(li);
+    });
+};
+
+store.subscribe(() => console.log(store.getState()));
+store.subscribe(paintToDos);
+
+const onSubmit = (e) => {
+    e.preventDefault();
+    const toDo = input.value;
+    input.value = "";
+    dispatchAddToDo(toDo);
+};
+
+form.addEventListener("submit", onSubmit);
